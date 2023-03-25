9f1f13c82f1fe230d5a37eefdb76e6a188b35416
nawed2611 <nawed2611@gmail.com>
2023-03-25 17:37:48 +0530

    Add changes to prisma types

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,17 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient({
-    log: ["query", "info", "warn"],
-  });
+  prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
